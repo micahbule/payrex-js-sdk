19fb5ba423a3358d9fb8e20cb9723066111c756d
Micah Andrew Bule <micah.bule@gmail.com>
2024-05-25 16:08:20 +0800

    feat: add refund to exported client

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import HttpClient from "./http";
 import PaymentIntentService from "./payment-intent/service";
+import RefundService from "./refund/service";
 
 export default class PayRexClient {
   paymentIntent: PaymentIntentService;
+  refund: RefundService;
 
   constructor(secretApiKey: string) {
     const httpClient = new HttpClient(secretApiKey);
 
     this.paymentIntent = new PaymentIntentService(httpClient);
+    this.refund = new RefundService(httpClient);
   }
 }
