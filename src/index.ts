439684fb2ce39a8af7eed6f92db3e2f099f3d35a
Micah Andrew Bule <micah.bule@gmail.com>
2025-03-05 19:47:49 +0800

    feat: expose checkout session and customer service from main client

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import CheckoutSessionService from "./checkout-session/service";
+import CustomerService from "./customer/service";
 import HttpClient from "./http";
 import PaymentIntentService from "./payment-intent/service";
 import RefundService from "./refund/service";
@@ -7,6 +9,8 @@ export default class PayRexClient {
 	paymentIntent: PaymentIntentService;
 	refund: RefundService;
 	webhook: WebhookService;
+	checkoutSession: CheckoutSessionService;
+	customer: CustomerService;
 
 	constructor(secretApiKey: string) {
 		const httpClient = new HttpClient(secretApiKey);
@@ -14,5 +18,7 @@ export default class PayRexClient {
 		this.paymentIntent = new PaymentIntentService(httpClient);
 		this.refund = new RefundService(httpClient);
 		this.webhook = new WebhookService(httpClient);
+		this.checkoutSession = new CheckoutSessionService(httpClient);
+		this.customer = new CustomerService(httpClient);
 	}
 }
