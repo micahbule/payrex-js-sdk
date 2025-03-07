b1dc0b781a3b69842b3c2657f25060095248e608
Micah Andrew Bule <micah.bule@gmail.com>
2025-03-07 20:29:34 +0800

    feat: initial implementation for billing statement

--- src/billing-statement/service.ts
+++ src/billing-statement/service.ts
@@ -1,10 +1,34 @@
 import type HttpClient from "../http";
 import type { BaseService } from "../types";
+import { formatRequestPayload } from "../utils";
+import BillingStatementDto from "./dto";
+import type {
+	CreateBillingStatementPayload,
+	UpdateBillingStatementPayload,
+} from "./types";
 
-export default class CheckoutSessionService implements BaseService {
+export default class BillingStatementService implements BaseService {
 	basePath = "/billing_statements";
 
 	constructor(private readonly client: HttpClient) {}
 
-	async create() {}
+	async create(params: CreateBillingStatementPayload) {
+		const response = await this.client.send(
+			"post",
+			this.basePath,
+			formatRequestPayload(params),
+		);
+
+		return new BillingStatementDto(response.body);
+	}
+
+	async update(params: UpdateBillingStatementPayload) {
+		const { id, ...payload } = params;
+		const response = await this.client.send(
+			"put",
+			`${this.basePath}/${id}`,
+			formatRequestPayload(payload),
+		);
+		return new BillingStatementDto(response.body);
+	}
 }
---
+++ src/billing-statement/types.ts
@@ -0,0 +1,42 @@
+import type { CustomerResource } from "../customer/types";
+import type { PaymentIntentResource } from "../payment-intent/types";
+import type { AvailablePaymentMethods } from "../types";
+
+export type CreateBillingStatementPayload = {
+	customer_id: string;
+	description?: string;
+	billing_details_collection?: string;
+	payment_settings: {
+		payment_methods: AvailablePaymentMethods;
+	};
+	metadata?: Record<string, string>;
+};
+
+export type UpdateBillingStatementPayload =
+	Partial<CreateBillingStatementPayload> & {
+		id: string;
+	};
+
+export type BillingStatementResource = {
+	id: string;
+	resource: string;
+	amount: number;
+	currency: string;
+	customer_id: string;
+	description: string | null;
+	status: string;
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	line_items: any[]; // Fix typing
+	livemode: boolean;
+	url: string;
+	customer: Pick<CustomerResource, "id" | "name" | "email">;
+	payment_intent: PaymentIntentResource;
+	metadata: Record<string, string> | null;
+	payment_settings: {
+		payment_methods: AvailablePaymentMethods;
+	};
+	billing_details_collection: string;
+	due_at: number;
+	created_at: number;
+	updated_at: number;
+};
